0d9fab3a20c328a618a022ead53ca7fbb2794f10
Leonardo <leonard67848953@gmail.com>
2025-10-29 12:18:03 -0400

    .

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -44,7 +44,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.edit',$request->user())->with('status', 'profile-updated');
     }
 
     //metodo para eliminar el perfil
