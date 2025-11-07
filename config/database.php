e257de259a9131d5e79c5c9b2e12efc790b0d869
Leonardo <leonard67848953@gmail.com>
2025-11-07 04:11:56 -0500

    solucion error

--- config/database.php
+++ config/database.php
@@ -95,7 +95,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => extension_loaded('pdo_pgsql') ? (env('DB_SSLMODE') === 'require' ? [
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ] : []) : [],
         ],
 
         'sqlsrv' => [
